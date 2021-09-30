9aed2c0ce6162c480cce7141d646c4ad9d030058
thisisshi <theli@outlook.com>
2021-09-30 11:23:20 -0700

    update

--- deploy/scripts/parse_output.py
+++ deploy/scripts/parse_output.py
@@ -43,10 +43,7 @@ def make_comment(commit, resource_counts):
         ])
 
         for account in v['accounts'].keys():
-            for region in v['accounts'].keys():
-                log.info(account)
-                log.info(region)
-                log.info(v['accounts'][account])
+            for region in v['accounts'][account].keys():
                 new = v['accounts'][account][region]['new']
                 original = v['accounts'][account][region]['original']
                 delta = get_delta_string(
