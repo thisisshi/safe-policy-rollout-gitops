f5a4544ed42e969f897216ef19d57ea17ad0284e
thisisshi <theli@outlook.com>
2021-09-30 15:31:45 -0700

    docs - update docs

--- projects/ci/main.tf
+++ projects/ci/main.tf
@@ -2,17 +2,22 @@ variable "github_token" {
   type = string
 }
 
-provider "aws" {}
-
 module "c7n_ci" {
   source = "github.com/thisisshi/terraform-aws-c7n-ci"
 
+  # github token
   github_token   = var.github_token
+  # repository url
   repository_url = "https://github.com/thisisshi/safe-policy-rollout-gitops.git"
+  # location of buildspec.yaml, defaults to buildspec.yaml
   buildspec      = "buildspec.yaml"
+  # branch to compare pr results to
   base_branch    = "main"
+  # repo name
   github_repo    = "thisisshi/safe-policy-rollout-gitops"
+  # absolute path of the accounts.yaml file you created
   accounts_yaml  = "/Users/sonny/dev/thisisshi/gitops-policy-rollout/accounts.yaml"
+  # tags to apply to resource
   tags = {
     Owner = "sonny@stacklet.io"
     Env   = "dev"
